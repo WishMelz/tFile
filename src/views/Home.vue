711c278158f57cef15a7420e69ab50a5f7bd7d5c
WishMeLz <i@itsse.cn>
2021-11-17 17:28:11 +0800

    page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
           drag
           multiple
           action="/"
-          :before-upload="befUpload"    
+          :before-upload="befUpload"     
         >
           <i class="el-icon-upload"></i>
           <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
